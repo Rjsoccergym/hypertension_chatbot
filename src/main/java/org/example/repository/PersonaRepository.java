fbc282a3df14ba321e8d9289576c2f74a084e525
SIISA-TOC <jrobledoacevedo@gmail.com>
2026-03-24 23:03:15 -0500

    Resolved: Remove this unused import 'org.example.model.entity.TipoIdentificacion'.

--- src/main/java/org/example/repository/PersonaRepository.java
+++ src/main/java/org/example/repository/PersonaRepository.java
@@ -1,7 +1,6 @@
 package org.example.repository;
 
 import org.example.model.entity.Persona;
-import org.example.model.entity.TipoIdentificacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
